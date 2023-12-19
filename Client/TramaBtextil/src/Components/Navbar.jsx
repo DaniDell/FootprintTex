d000d039c1214301add179c1e6b641878048c4c2
Daniela Dell Acqua <ddacqua@gmail.com>
2023-12-19 01:17:28 -0300

    ok

--- Client/TramaBtextil/src/Components/Navbar.jsx
+++ Client/TramaBtextil/src/Components/Navbar.jsx
@@ -8,15 +8,14 @@ const Navbar = () => {
     return (
         <AppBar position="sticky">
             <Toolbar>
-                
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
-                Caluladora de reciclaje textil
+                    Caluladora de reciclaje textil
                 </Typography>
                 {/* Puedes agregar enlaces de navegación aquí */}
-               
                 <IconButton edge="start" color="inherit" aria-label="menu">
-                <img src="/logo.svg" alt="Logo" style={{ marginRight: '10px', height: '50px', flexGrow: 1, color: 'inherit' }} />
-                   
+                    <a href="https://sites.google.com/view/tramabtextil/inicio" target="_blank" rel="noopener noreferrer">
+                        <img src="/logo.svg" alt="Logo" style={{ marginRight: '10px', height: '50px', flexGrow: 1, color: 'inherit' }} />
+                    </a>
                 </IconButton>
             </Toolbar>
         </AppBar>
