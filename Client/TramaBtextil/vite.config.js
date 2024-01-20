b3fed8b8516bac5742c328baa9123cf549790839
Daniela Dell Acqua <ddacqua@gmail.com>
2024-01-20 15:41:49 -0300

    update dependencias

--- Client/TramaBtextil/vite.config.js
+++ Client/TramaBtextil/vite.config.js
@@ -1,11 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      deny: ['.env', '.env.*', '*.{crt,pem}', 'custom.secret']
+    }
+  },
   build: {
     sourcemap: true,
   },
-})
+});
