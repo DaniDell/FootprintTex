9fffaca979c68619e81da9deac89b025564c4a89
Daniela Dell Acqua <ddacqua@gmail.com>
2024-01-28 13:20:37 -0300

    better UX errors

--- Client/TramaBtextil/src/Components/Utils/InputComponent.jsx
+++ Client/TramaBtextil/src/Components/Utils/InputComponent.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import { FormControl, InputLabel, Select, MenuItem, Button, Grid, FormHelperText } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem, Button, Grid, FormHelperText, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Box } from '@mui/material';
 
 const InputComponent = ({ onInputChange, onTextileCompositionChange, onCalculateClick })  => {
 
@@ -10,40 +9,71 @@ const InputComponent = ({ onInputChange, onTextileCompositionChange, onCalculate
   const [isValid, setIsValid] = useState(false);
   const [error, setError] = useState('');
   const [compositionError, setCompositionError] = useState('');
+  const [open, setOpen] = useState(false);
+  const [newKilosError, setNewKilosError] = useState('');
 
   useEffect(() => {
-    setIsValid(composition !== '' && kilos !== '' && kilos >= 0);
+    setIsValid(composition !== '' && kilos !== '' && kilos >= 0.001);
   }, [composition, kilos]);
 
   const handleKilosChange = (e) => {
     const value = e.target.value;
-    if (!isNaN(value) && value >= 0 && !value.includes('e')) {
+    if (value === '' || isNaN(value) || value < 0 || value.includes('e')) {
+      setError('El valor debe ser un número mayor o igual a 0 y no puede contener la letras.');
+    } else {
       setKilos(Number(value));
       onInputChange(Number(value));
       setError('');
-    } else {
-      setError('El valor debe ser un número mayor o igual a 0 y no puede contener la letras.');
     }
   };
 
   const handleCompositionChange = (e) => {
     const value = e.target.value;
-    if (value !== '') {
+    if (value === '') {
+      setCompositionError('Debe seleccionar una composición textil.');
+    } else {
       setComposition(value);
       onTextileCompositionChange(value);
       setCompositionError('');
-    } else {
-      setCompositionError('Debe seleccionar una composición textil.');
     }
   };
 
+
+
+
+
   const handleCalculateClick = () => {
-    onCalculateClick();
-    setTimeout(() => {
-      document.body.scrollIntoView({ behavior: 'smooth', block: 'end' });
-    }, 0);
+    let isValid = true;
+    let newCompositionError = '';
+    let newKilosError = '';
+  
+    if (!composition) {
+      isValid = false;
+      newCompositionError = 'Seleccione una composición.';
+    }
+  
+    if (!kilos) {
+      isValid = false;
+      newKilosError = 'Introduzca los kilos.';
+    }
+  
+    setCompositionError(newCompositionError);
+    setNewKilosError(newKilosError);
+  
+    if (!isValid) {
+      setOpen(true);
+    } else {
+      onCalculateClick();
+      setTimeout(() => {
+        document.body.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }, 0);
+    }
   };
   
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Box mb={1.5}>
@@ -67,27 +97,47 @@ const InputComponent = ({ onInputChange, onTextileCompositionChange, onCalculate
       </Box>
       <Grid container spacing={2} alignItems="center">
         <Grid item xs={8}>
-          <TextField 
-            type="number"
-            onChange={handleKilosChange}
-            onKeyPress={(event) => {
-              if (!/[0-9]/.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-            label="kilos gestionados"
-            fullWidth
-            size="small"
-            error={!!error}
-            helperText={error}
-          />
+        <TextField 
+  type="number"
+  onChange={handleKilosChange}
+  onKeyPress={(event) => {
+    if (!/[0-9]/.test(event.key)) {
+      event.preventDefault();
+    }
+  }}
+  label="kilos gestionados"
+  fullWidth
+  size="small"
+  error={!!newKilosError}
+  helperText={newKilosError}
+/>
         </Grid>
+     
         <Grid item xs={4}>
-          <Button fullWidth variant="contained" color="primary" onClick={handleCalculateClick} disabled={!isValid}>
-            Calcular
-          </Button>
-        </Grid>
+  <Button fullWidth variant="contained" color="primary" onClick={handleCalculateClick} >
+    Calcular
+  </Button>
+  <Dialog
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">{"Error"}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        Por favor, rellene todos los campos correctamente
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} color="primary" autoFocus>
+        Ok
+      </Button>
+    </DialogActions>
+  </Dialog>
+</Grid>
       </Grid>
+
     </div>
   );
 };
--- Client/TramaBtextil/src/Views/Calculate.jsx
+++ Client/TramaBtextil/src/Views/Calculate.jsx
@@ -6,7 +6,7 @@ const Calculate = () => {
     
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100vw', height: '100vh'}}>
             <Suspense fallback={<CircularProgress />}>
                 <DemoCalculator />
             </Suspense>
