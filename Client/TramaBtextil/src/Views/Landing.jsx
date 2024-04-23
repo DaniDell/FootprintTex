e177ae2261529bbde7648ed3a243ce08bc2eb1bd
Daniela Dell Acqua <ddacqua@gmail.com>
2024-04-23 16:18:37 -0300

    final update

--- Client/TramaBtextil/src/Views/Landing.jsx
+++ Client/TramaBtextil/src/Views/Landing.jsx
@@ -71,7 +71,7 @@ const Landing = () => {
           <Suspense fallback={<div></div>}>
         {showCalculate && <Calculate />}
       </Suspense>
-      {show && <div>"Solo podemos mejorar aquello que somos capaces de medir📏"</div>}
+      {show && <div>❝Solo podemos mejorar aquello que somos capaces de medir📏❞</div>}
           </div>
         </div>
       </Container>
