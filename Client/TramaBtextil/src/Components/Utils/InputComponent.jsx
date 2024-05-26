13b4b1dfa58aca564d4949582403d10c4d8edf34
Daniela Dell Acqua <ddacqua@gmail.com>
2024-05-26 13:43:44 -0300

    Update InputComponent.jsx

--- Client/TramaBtextil/src/Components/Utils/InputComponent.jsx
+++ Client/TramaBtextil/src/Components/Utils/InputComponent.jsx
@@ -101,19 +101,19 @@ const InputComponent = ({
     <div  style={{
       justifyContent:  "center",
       alignItems: "center",
-      margin: 0, // Quitar los márgenes
-      padding: 0, // Quitar el padding
+      margin: 0, 
+     
     }}>
 {!isSubmitted && (
 <Paper 
   elevation={0} 
   style={{ 
     marginBottom: '1rem', 
-    padding: '0rem 0.2rem 0rem 0.2rem',
+    padding: '0rem 0.8rem 0rem 0.8rem',
     backgroundColor: '#E3E461AA', 
     borderRadius: '5px',
    border: '2px solid #E3E461AA',
-   margin: '0rem 0rem 1rem 0rem', 
+   margin: '0rem 0rem 0.8rem 0rem', 
   
   }}
 >
@@ -130,7 +130,7 @@ Con cada corte industrial se genera hasta un <strong>15%</strong> de merma (reta
 
 </Paper>
 
-)}     <Box mb={1.5}  margin={0} padding={0}>
+)}     <Box mb={1.5}  margin={0} >
         
         <FormControl fullWidth error={!!compositionError}>
           <InputLabel id="textile-composition-label">
@@ -141,7 +141,7 @@ Con cada corte industrial se genera hasta un <strong>15%</strong> de merma (reta
             label="Composición base fibra textil"
             value={composition}
             onChange={handleCompositionChange}
-            sx={{  textAlign: "left" }}
+            sx={{  textAlign: "left", marginBottom: "0.8rem" }}
           >
             <MenuItem
               value="algodon Reciclado"
