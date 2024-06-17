0273bee5bb7b880d0c75a7c5208fe35d91bdc43e
Daniela Dell Acqua <ddacqua@gmail.com>
2024-06-17 17:20:16 -0300

    Update theme.js

--- Client/TramaBtextil/src/theme.js
+++ Client/TramaBtextil/src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     background: {
-      default: '#FAFAFA',
+      default: '#D0C9C8',
     },
     text: {
       primary: '#474744', 
